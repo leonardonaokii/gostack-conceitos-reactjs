e7cf110f0a8c4a125906ad8e32f7dde8b2d9fe34
leonardonaokii <leonardonaokii@gmail.com>
2021-03-24 18:55:48 -0300

    Challenge completed! :tada:

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+
+      setRepositories(response.data);
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+
+    const repository = {
+      title,
+      url,
+      techs: techs.split(',').map(tech => tech.trim())
+    }
+
+    const response = await api.post('repositories', repository)
+
+    setRepositories(prevRepo => [...prevRepo, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`)
+      setRepositories(prevRepo => prevRepo.filter(repo => repo.id !== id))
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      <form onSubmit={handleAddRepository}>
+        <input onChange={event => setTitle(event.target.value)} type="text" placeholder="title" />
+        <input onChange={event => setUrl(event.target.value)} type="text" placeholder="url" />
+        <input onChange={event => setTechs(event.target.value)} type="text" placeholder="techs" />
+        <button type="submit">Adicionar</button>
+      </form>
 
-          <button onClick={() => handleRemoveRepository(1)}>
+
+      <ul data-testid="repository-list">
+        {repositories.map(repository =>
+        (<li key={repository.id}>
+          {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li>)
+        )
+        }
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
